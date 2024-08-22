a6ccfaaa2ece5db35ea98c559cd3ddae405ff4d2
bian paraguya <121653299+BianPaulParaguya@users.noreply.github.com>
2024-08-22 14:26:59 -0700

    Update for duration of note played

--- ece425project.c
+++ ece425project.c
@@ -8,20 +8,32 @@
 void Delay(unsigned long counter); // used to add delay
 void UART5_Init(void); // Initialize UART5 module
 char UART5_Read(void); // Read data from Rx5 pin of TM4C123
-void UART5_Write(unsigned char data); // Transmit a character over Tx5 pin 
-void UART5_Write_String(char *str); // Transmit a string over Tx5 pin 
-void delayUs(int ttime); // Delay in microseconds
-void playNote(int note, int duration); // Play note
+void UART5_Write(unsigned char data); // Transmit a character over Tx5 pin
+void UART5_Write_String(char *str); // Transmit a string over Tx5 pin
+void playNoteContinuous(int note); // Play note continuously
+void stopNote(); // Stop playing note
+void TIMER1A_Handler(void); // Timer1A ISR
 
-// Note Frequencies for Do Re Mi Fa So La Ti Do
+// Note Frequencies for C4 to C5
 #define C4  261
+#define Db4 277
 #define D4  294
+#define Eb4 311
 #define E4  329
 #define F4  349
+#define Gb4 370
 #define G4  392
+#define Ab4 415
 #define A4  440
+#define Bb4 466
 #define B4  494
 #define C5  523
+#define Db5 554
+#define D5  587
+#define Eb5 622
+#define E5  659
+
+volatile int currentNote = 0; // To track the current note being played
 
 int main(void) {
     UART5_Init(); // Call UART5_Init() to initialize UART5 of TM4C123GH6PM
@@ -30,80 +42,113 @@ int main(void) {
     SYSCTL->RCGCGPIO |= 0x20;   /* Enable clock to GPIOF */
     GPIOF->DIR |= 0x0E;         // Set PF1, PF2 and PF3 as digital output pins
     GPIOF->DEN |= 0x0E;         // Enable PF1, PF2 and PF3 as digital GPIO pins
-    Delay(10); 
-    
+    Delay(10);
+   
     /* Set PC4 as output pin for buzzer */
     SYSCTL->RCGCGPIO |= 0x04;   /* Enable clock to GPIOC */
     GPIOC->AMSEL &= ~0x10;      /* Disable analog function */
     GPIOC->DIR |= 0x10;         /* Set PC4 as output pin */
     GPIOC->DEN |= 0x10;         /* Set PC4 as digital pin */
-    
-    /* Initialize TIMER0 for delays */
-    SYSCTL->RCGCTIMER |= 1;     /* Enable clock to TIMER0 */
-    TIMER0->CTL = 0;            /* Disable TIMER0 during setup */
-    TIMER0->CFG = 0x04;         /* 16-bit timer mode */
-    TIMER0->TAMR = 0x02;        /* Periodic mode */
-    TIMER0->TAILR = 16 - 1;     /* Interval load value for 1us ticks */
-    TIMER0->ICR = 0x1;          /* Clear the TimerA timeout flag */
-    TIMER0->CTL |= 0x01;        /* Enable TIMER0A */
-    
-    UART5_Write_String("UART5 Initialized. Type something:\r\n");
+   
+    /* Initialize TIMER1 for buzzer */
+    SYSCTL->RCGCTIMER |= 2;     /* Enable clock to TIMER1 */
+    TIMER1->CTL = 0;            /* Disable TIMER1 during setup */
+    TIMER1->CFG = 0x04;         /* 16-bit timer mode */
+    TIMER1->TAMR = 0x02;        /* Periodic mode */
+    TIMER1->TAILR = 16000000 / C4 - 1;  /* Initial interval load value for C4 */
+    TIMER1->ICR = 0x1;          /* Clear the TimerA timeout flag */
+    TIMER1->IMR |= 0x01;        /* Enable TimerA timeout interrupt */
+    TIMER1->CTL |= 0x01;        /* Enable TIMER1A */
 
-    char buffer[BUFFER_SIZE];
-    int bufferIndex = 0;
+    NVIC->ISER[0] = 1 << 21;    /* Enable IRQ21 for TIMER1A */
+
+    UART5_Write_String("UART5 Initialized. Type something:\r\n");
 
     while (1) {
         /* Read a character from UART5 */
         if ((UART5->FR & (1 << 4)) == 0) {  /* If the RX buffer is not empty */
-            buffer[bufferIndex] = UART5_Read();
-            bufferIndex = (bufferIndex + 1) % BUFFER_SIZE;
-        }
-
-        /* Process characters in the buffer */
-        if (bufferIndex > 0) {
-            for (int i = 0; i < bufferIndex; i++) {
-                char c = buffer[i];
-                
-                /* Play note based on received character */
+            char c = UART5_Read();
+           
+            if (c == 'x') {
+                stopNote();
+                UART5_Write_String("Stopping Note\n");
+            } else {
                 switch(c) {
                     case 'C':
-                        playNote(C4, 500); // 500ms for better duration
-                        UART5_Write_String("Playing C4 (Do)\n");
+                        playNoteContinuous(C4);
+                        UART5_Write_String("Playing C4 (C)\n");
+                        break;
+                    case 'd':
+                        playNoteContinuous(Db4);
+                        UART5_Write_String("Playing Db4 (Db)\n");
                         break;
                     case 'D':
-                        playNote(D4, 500);
-                        UART5_Write_String("Playing D4 (Re)\n");
+                        playNoteContinuous(D4);
+                        UART5_Write_String("Playing D4 (D)\n");
+                        break;
+                    case 'e':
+                        playNoteContinuous(Eb4);
+                        UART5_Write_String("Playing Eb4 (Eb)\n");
                         break;
                     case 'E':
-                        playNote(E4, 500);
-                        UART5_Write_String("Playing E4 (Mi)\n");
+                        playNoteContinuous(E4);
+                        UART5_Write_String("Playing E4 (E)\n");
                         break;
                     case 'F':
-                        playNote(F4, 500);
-                        UART5_Write_String("Playing F4 (Fa)\n");
+                        playNoteContinuous(F4);
+                        UART5_Write_String("Playing F4 (F)\n");
+                        break;
+                    case 'g':
+                        playNoteContinuous(Gb4);
+                        UART5_Write_String("Playing Gb4 (Gb)\n");
                         break;
                     case 'G':
-                        playNote(G4, 500);
-                        UART5_Write_String("Playing G4 (So)\n");
+                        playNoteContinuous(G4);
+                        UART5_Write_String("Playing G4 (G)\n");
+                        break;
+                    case 'a':
+                        playNoteContinuous(Ab4);
+                        UART5_Write_String("Playing Ab4 (Ab)\n");
                         break;
                     case 'A':
-                        playNote(A4, 500);
-                        UART5_Write_String("Playing A4 (La)\n");
+                        playNoteContinuous(A4);
+                        UART5_Write_String("Playing A4 (A)\n");
+                        break;
+                    case 'b':
+                        playNoteContinuous(Bb4);
+                        UART5_Write_String("Playing Bb4 (Bb)\n");
                         break;
                     case 'B':
-                        playNote(B4, 500);
-                        UART5_Write_String("Playing B4 (Ti)\n");
+                        playNoteContinuous(B4);
+                        UART5_Write_String("Playing B4 (B)\n");
                         break;
                     case 'c':
-                        playNote(C5, 500);
-                        UART5_Write_String("Playing C5 (Do)\n");
+                        playNoteContinuous(C5);
+                        UART5_Write_String("Playing C5 (C)\n");
                         break;
+case '1':
+playNoteContinuous(Db5);
+                        UART5_Write_String("Playing Db5 (Db)\n");
+break;
+case '2':
+playNoteContinuous(D5);
+                        UART5_Write_String("Playing D5 (D)\n");
+break;
+case '3':
+playNoteContinuous(Eb5);
+UART5_Write_String("Playing Eb5 (Eb)\n");
+break;
+case '4':
+ playNoteContinuous(E5);
+                        UART5_Write_String("Playing E5 (E)\n");
+break;
                     default:
                         UART5_Write_String("Unknown command\n");
                         break;
                 }
+
+                Delay(500000); // Delay for 500ms after each button press
             }
-            bufferIndex = 0;  // Reset buffer index after processing
         }
     }
 }
@@ -151,22 +196,22 @@ void Delay(unsigned long counter) {
     for (i = 0; i < counter; i++);
 }
 
-void delayUs(int ttime) {
-    int i;
-    for (i = 0; i < ttime; i++) {
-        while ((TIMER0->RIS & 0x1) == 0); // wait for TimerA timeout flag
-        TIMER0->ICR = 0x1; // clear the TimerA timeout flag
-    }
+void playNoteContinuous(int note) {
+    currentNote = note;
+    int halfPeriod = 16000000 / (2 * note); // Calculate half period in timer ticks
+    TIMER1->TAILR = halfPeriod - 1;        // Load the half period value into the timer
+    TIMER1->CTL |= 0x01;                   // Enable TIMER1A
 }
 
-void playNote(int note, int duration) {
-    int halfPeriod = 1000000 / (2 * note); 
-    int cycles = (duration * 1000) / (2 * halfPeriod);
+void stopNote() {
+    currentNote = 0;
+    GPIOC->DATA &= ~0x10; // Ensure the buzzer is off
+    TIMER1->CTL &= ~0x01; // Disable TIMER1A
+}
 
-    for (int i = 0; i < cycles; i++) { 
-        GPIOC->DATA |= 0x10; // PC4: HIGH
-        delayUs(halfPeriod);
-        GPIOC->DATA &= ~0x10; // PC4: LOW
-        delayUs(halfPeriod);
+void TIMER1A_Handler(void) {
+    TIMER1->ICR = 0x1; // Clear the TimerA timeout flag
+    if (currentNote != 0) {
+        GPIOC->DATA ^= 0x10; // Toggle PC4
     }
 }
